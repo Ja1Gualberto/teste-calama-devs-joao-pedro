3c11407cf887a7521afa6ed22c7444be02896af0
Ja1Gualberto <gualberto.silvaj@gmail.com>
2026-04-04 22:01:11 -0300

    feat: Adiciona novos componentes e atualiza estilos da página

--- João Pedro/index.js
+++ João Pedro/index.js
@@ -1,11 +1,13 @@
 function marcarLinkAtual() {
   const url_atual = window.location.href;
   const nav_links = document.querySelectorAll('.navbar .elementos a');
-  
+    
   nav_links.forEach(link => {
-    if (link.href === url_atual) {
+    if (link.href != url_atual) {
       link.classList.remove('text-decoration-none');
       link.classList.add('link-ativo');
+      console.log(link.href, url_atual);
+      
     }
   });
 }
